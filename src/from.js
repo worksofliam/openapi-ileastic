1486c04ad97c7c31494da972a0d574a01f73d026
worksofliam <mrliamallan@live.co.uk>
2020-07-03 15:15:13 +0100

    Fix into/from procedures

--- src/from.js
+++ src/from.js
@@ -12,15 +12,16 @@ module.exports = class From {
     lines.push(`  Dcl-Pi *N Pointer;`, `    ${structName} LikeDS(${structName}_t);`, `  End-Pi;`, ``);
     lines.push(`  Dcl-S lIndex Int(5);`);
     lines.push(`  Dcl-S lArray Pointer;`);
+    lines.push(`  Dcl-S lObject Pointer;`);
     lines.push(`  Dcl-S lDocument Pointer;`, '');
     lines.push(`  lDocument = JSON_NewObject();`);
   
-    this.generateContents(object, structName);
+    this.generateContents(object, structName, 'lDocument');
   
     lines.push(``, `  Return lDocument;`, `End-Proc;`, ``);
   }
 
-  static generateContents(object, structName) {
+  static generateContents(object, structName, variable) {
     const setTypes = {
       'number': `SetNum`,
       'string': 'SetStr',
@@ -37,7 +38,7 @@ module.exports = class From {
         case 'string':
         case 'boolean':
         case 'integer':
-          lines.push(`  JSON_${setTypes[currentProperty.type]}(lDocument:'${name}':${structName}.${name});`);
+          lines.push(`  JSON_${setTypes[currentProperty.type]}(${variable}:'${name}':${structName}.${name});`);
           break;
   
         case 'object':
@@ -46,18 +47,31 @@ module.exports = class From {
           break;
   
         case 'array':
-          lines.push(
-            ``,
-            `  For lIndex = 1 to ${structName}.${name}_len;`,
-          );
   
           if (currentProperty.items.type === "object") {
-            this.generateContents(currentProperty.items, `${structName}.${name}(lIndex)`);
+            lines.push(
+              ``,
+              `  lArray = JSON_NewArray();`,
+              `  For lIndex = 1 to ${structName}.${name}_len;`,
+              `    lObject = JSON_NewObject();`
+            );
+
+            this.generateContents(currentProperty.items, `${structName}.${name}(lIndex)`, `lObject`);
+            lines.push(
+              `    JSON_ArrayPush(lArray:lObject);`,
+              `  Endfor;`,
+              `  JSON_SetPtr(${variable}:'${name}':lArray);`
+            );
           } else {
-            lines.push(`    JSON_${setTypes[currentProperty.items.type]}(${structName}.${name}:'[]':${structName}.${name});`);
+            
+            lines.push(
+              ``,
+              `  For lIndex = 1 to ${structName}.${name}_len;`,
+              `    JSON_${setTypes[currentProperty.items.type]}(${variable}:'${name}[]':${structName}.${name});`,
+              `  Endfor;`
+            );
           }
-  
-          lines.push(`  Endfor;`);
+
           break;
       }
     }
--- src/into.js
+++ src/into.js
@@ -11,14 +11,15 @@ module.exports = class Into {
     lines.push(`Dcl-Proc into_${structName};`);
     lines.push(`  Dcl-Pi *N LikeDS(${structName}_t);`, `    pDocument Pointer;`, `  End-Pi;`, ``);
     lines.push(`  Dcl-DS ${structName} LikeDS(${structName}_t);`);
-    lines.push(`  Dcl-DS list likeds(JSON_ITERATOR);`, ``);
+    lines.push(`  Dcl-DS list likeds(JSON_ITERATOR);`);
+    lines.push(`  Dcl-S lNode Pointer;`)
   
-    this.generateContents(object, structName);
+    this.generateContents(object, structName, 'pDocument');
   
     lines.push(``, `  Return ${structName};`, `End-Proc;`, ``);
   }
 
-  static generateContents(object, structName) {
+  static generateContents(object, structName, variable) {
     const getTypes = {
       'number': `GetNum`,
       'string': 'GetValuePtr',
@@ -35,23 +36,23 @@ module.exports = class Into {
         case 'string':
         case 'boolean':
         case 'integer':
-          lines.push(`  ${structName}.${name} = JSON_${getTypes[currentProperty.type]}(pDocument:'${name}');`);
+          lines.push(`  ${structName}.${name} = JSON_${getTypes[currentProperty.type]}(${variable}:'${name}');`);
           break;
   
         case 'object':
-          lines.push('');
-          this.generateContents(currentProperty, structName + "." + name);
+          lines.push('', `  lNode = JSON_Locate(${variable}:'${name}');`);
+          this.generateContents(currentProperty, structName + "." + name, `lNode`);
           break;
   
         case 'array':
           lines.push(
             ``,
-            `  list = json_SetIterator(JSON_Locate(pDocument:'${name}'));`,
+            `  list = json_SetIterator(JSON_Locate(${variable}:'${name}'));`,
             `  Dow json_ForEach(list);`,
           );
   
           if (currentProperty.items.type === "object") {
-            this.generateContents(currentProperty.items, `${structName}.${name}(list.count)`);
+            this.generateContents(currentProperty.items, `${structName}.${name}(list.count)`, 'list.this');
           } else {
             lines.push(`    ${structName}.${name}(list.count) = JSON_${getTypes[currentProperty.items.type]}(list.this);`);
           }
